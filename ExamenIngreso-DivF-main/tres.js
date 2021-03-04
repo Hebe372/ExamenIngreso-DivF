387f34de3d1c18e116513996df4c558d1b4400eb
Hebe372 <60760295+Hebe372@users.noreply.github.com>
2021-03-03 21:29:05 -0300

    Add files via upload

--- ExamenIngreso-DivF-main/tres.js
+++ ExamenIngreso-DivF-main/tres.js
@@ -1,4 +1,102 @@
 function mostrar()
 {
-	alert("tres");
+	function mostrar()
+{
+let nombre;
+let obraSocial;
+let edad;
+let temperaruta;
+let sexo;
+let precio = 600;
+let contPersonasOsde = 0;
+let minEdad;
+let minNombre;
+let minTemperatura;
+let contPasajerosOtras = 0;
+let contPasajerosOsde = 0;
+let contPasajerosPami = 0;
+let totalPasajeros;
+let precioFinal;
+let seguir;
+flag = 1;
+	
+
+	do{
+
+		nombre = prompt("Ingrese nombre: ");
+    	while(!isNaN(nombre)){
+        nombre = prompt("Error. Ingrese nombre: ");
+    	}
+
+		obraSocial=prompt("Ingrese el tipo de producto (PAMI - OSDE - OTRAS): ").toLocaleUpperCase;
+		while(obraSocial !="PAMI" && obraSocial != "OSDE" && obraSocial != "OTRAS"){
+			tipo=prompt("Error. Ingrese el tipo de producto (PAMI - OSDE - OTRAS): ").toLocaleUpperCase;
+    	}
+		edad = parseInt(prompt("Ingrese la edad "));
+    	while(isNaN(edad) || edad < 17){
+        edad = parseInt(prompt("Error. Ingrese la edad "));
+    	}
+
+		temperatura = parseFloat(prompt("Ingrese la temperatura "));
+    	while(temperatura <=0 || isNan(temperatura)){
+        temperatura = parseFloat(prompt("Error. Ingrese la temperatura "))
+    	}
+
+		sexo = prompt("Ingrese el sexo (f - m)").toLocaleLowerCase();
+		while(sexo != "m" && sexo != "f"){
+		sexo = prompt("Error. Ingrese el sexo (f - m)".toLocaleLowerCase());
+		}
+
+		// a) La cantidad de personas con OSDE de mas de 60 años.
+		if(obraSocial == "OSDE" && edad > 60){
+			contPersonasOsde++;
+		}
+
+	// b) el nombre y temperatura de la mujer PAMI mas joven.
+	if(obraSocial == "PAMI" && (flag || edad<minEdad)){
+		minEdad = edad;
+		minTemperatura = temperaruta;
+		minNombre = nombre;
+		flag = 0;
+	}
+
+		switch(obraSocial){
+		case "PAMI":
+		contPasajerosPami++;
+		break;
+		case "OSDE":
+		contPasajerosOsde++;
+		break;
+		case "OTRAS":
+		contPasajerosOtras++;
+		break;
+		}
+
+	// d) si hay mas del 50% de los pasajeros que pertenecen a PAMI, 
+	//el precio final tiene un descuento del 25%, que solo mostramos si corresponde.
+
+	seguir = prompt("Desea continuar (s/n)? ");
+	} while(seguir == "s");
+
+// a) La cantidad de personas con OSDE de mas de 60 años.
+console.log ("La cantidad de personas con OSDE de mas de 60 años" + contPersonasOsde);
+
+// b) el nombre y temperatura de la mujer PAMI mas joven.
+console.log("el nombre y temperatura de la mujer PAMI mas joven es " + minNombre + " y " + minTemperatura + " ° ");
+
+// c) cuanto sale el viaje total sin descuento.
+totalPasajeros = contPasajerosPami + contPasajerosOsde + contPasajerosOtras
+console.log ("El viaje total sin descuento cuesta $ " + totalPasajeros * precio);
+
+// d) si hay mas del 50% de los pasajeros que pertenecen a PAMI, 
+//el precio final tiene un descuento del 25%, que solo mostramos si corresponde.
+
+
+if((totalPasajeros /2) < contPasajerosPami){
+	precioFinal = (precio * 0,5);
+
+}
+console.log("El precio final para pasajeros con PAMI es $ " + precioFinal);
+
+}//FIN DE LA FUNCIÓN
 }
--- ExamenIngreso-DivF-main/uno.js
+++ ExamenIngreso-DivF-main/uno.js
@@ -1,5 +1,147 @@
 
 function mostrar()
 {
-	alert("uno");
+	function mostrar()
+
+{
+	let nombre;
+	let tipo;
+	let precio;
+	let cantidad;
+	let categoria;
+	let fabricante;
+	let maxPrecio;
+	let maxFabricante;
+	let maxCategoria;
+	let acumCantAlcohol = 0;
+	let contCantAlcohol = 0;
+	let contCantIac = 0;
+	let acumCantIac = 0;
+	let acumCantBactericida = 0;
+	let contCantBactericida = 0;
+	let contCantDetergente = 0;
+	let acumCantDetergente = 0;
+	let contCantDesinfectante = 0;
+	let acumCantDesinfectante = 0;
+	let contCantQuat = 0;
+	let acumCantQuat = 0;
+	let totalCompraBactericida;
+	let totalCompraDesinfectante;
+	let totalCompraDetergente;
+	let contCantDetergenteMenos = 0;
+	let contCantDetergenteMenos = 0;
+	let promedioAlcohol;
+	let promedioIac;
+	let promedioQuat;
+	
+	flag = 1;
+	
+
+	for(var i=1; i=5; i++){
+
+		nombre = prompt("Ingrese nombre: ");
+    	while(!isNaN(nombre)){
+        nombre = prompt("Error. Ingrese nombre: ");
+    	}
+		
+		tipo=prompt("Ingrese el tipo de producto (ALCOHOL - IAC - QUAT): ").toLocaleUpperCase;
+		while(tipo !="ALCOHOL" && tipo!= "IAC" && tipo != "QUAT"){
+			tipo=prompt("Error. Ingrese el tipo de producto (ALCOHOL - IAC - QUAT): ").toLocaleUpperCase;
+		}
+
+		precio=parseFloat(prompt("Ingrese el precio (entre 100 y 300): "));
+		while(cantidad < 100 || cantidad > 300 || isNaN(precio)){
+			precio=prompt("Error. Ingrese el precio (entre 100 y 300): ");
+		}
+		
+		cantidad=parseInt(prompt("Ingrese la cantidad de unidades (entre 0 y 1000): "));
+		while(!(cantidad > 0 && cantidad <= 1000)){
+			cantidad=prompt("Error. Ingrese la cantidad de unidades (entre 0 y 1000): ");
+		}
+
+		categoria=prompt("Ingrese el tipo de producto (desinfectante - bactericida - detergente): ").toLocaleLowerCase;
+		while(categoria !="desinfectante" && categoria != "bactericida" && categoria != "detergente"){
+			categoria = prompt("Error. Ingrese el tipo de producto (ALCOHOL - IAC - QUAT): ").toLocaleLowerCase;
+		}
+		
+		fabricante=prompt("Ingrese el fabricante: ");
+
+		// a) El promedio de cantidad por tipo de producto
+
+		switch(tipo){
+			case "ALCOHOL":
+				contCantAlcohol++;
+				acumCantAlcohol += cantidad;
+				break;
+			case "IAC":
+				contCantIAC++;
+				acumCantIAC += cantidad;
+				break;
+			case "QUAT":
+				contCantQuat++;
+				acumCantQuat += cantidad;
+				break;
+		}
+	// b) La categoria con mas cantidad de unidades en total de la compra
+	switch(categoria){
+		case "desinfectante":
+			contCantDesinfectante++;
+			acumCantDesinfectante += cantidad;
+			totalCompraDesinfectante = precio * contCantDesinfectante;
+			break;
+		case "bactericida":
+			contCantBactericida++;
+			acumCantBactericida += cantidad;
+			totalCompraBactericida = precio * contCantBactericida;
+			break;
+		case "detergente":
+			contCantDetergente++;
+			acumCantDetergente += cantidad;
+			totalCompraDetergente = precio * contCantDetergente;
+			break;
+			
+	}
+	//c) Cuántas unidades de detergente con precios menos a 200 (inclusive) se compraron en total
+	if (categoria == detergente && precio <=200){
+	contCantDetergenteMenos++;
+	}
+	//d) el fabricante y Categoria del más caro de los productos
+	if(flag || precio>maxPrecio){
+		maxPrecio = precio;
+		maxFabricante = fabricante;
+		maxCategoria = categoria;
+		flag = 0;
+	}
+	}
+	
+	// a) El promedio de cantidad por tipo de producto
+	if(contCantAlcohol != 0){
+	promedioAlcohol = acumCantAlcohol/contCantAlcohol;
+	}
+	if(contCantIac != 0){
+	promedioIac =acumCantIac/contCantIac;
+	}
+	if(contCantQuat != 0){
+	promedioQuat = acumCantQuat/contCantQuat;
+	}
+console.log("El promedio de la cantidad de ALCOHOL es " + promedioAlcohol);
+console.log("El promedio de la cantidad de IAC es " + promedioIac);
+console.log("El promedio de la cantidad de QUAT es " + promedioQuat);
+
+if(totalCompraDesinfectante>totalCompraBactericida && totalCompraDesinfectante>totalCompraDetergente){
+	console.log ("La categoria con mas calidad de unidades en total de la compra es" + totalCompraDesinfectante);
+}
+if(totalCompraBactericida>totalCompraDesinfectante && totalCompraBactericida>totalCompraDetergente){
+	console.log ("La categoria con mas calidad de unidades en total de la compra es" + totalCompraBactericida);
+}
+if(totalCompraDetergente>totalCompraBactericida && totalCompraDetergente>totalCompraDesinfectante){
+	console.log ("La categoria con mas calidad de unidades en total de la compra es" + totalCompraDetergente);
+}
+//c) Cuántas unidades de detergente con precios menos a 200 (inclusive) se compraron en total
+console.log("unidades de detergente con precios menos a 200 (inclusive) se compraron en total " + contCantDetergenteMenos);
+
+
+//d) el fabricante y Categoria del más caro de los productos
+console.log("El producto mas caro, fabricante" + maxFabricante + "categoria " + maxCategoria);
+}//FIN DE LA FUNCIÓN
 }
